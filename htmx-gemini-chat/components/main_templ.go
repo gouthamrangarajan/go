e4614e4febe082c99491798b701ae61f63a00951
goutham rangarajan <gouthamrangarajan@gouthams-MacBook-Pro.local>
2025-06-15 08:42:39 -0400

    light and dark theme  kick start

--- htmx-gemini-chat/components/main_templ.go
+++ htmx-gemini-chat/components/main_templ.go
@@ -43,7 +43,7 @@ func Main(conversations []models.ChatConversation, sessions []models.ChatSession
 				}()
 			}
 			ctx = templ.InitializeContext(ctx)
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<main class=\"relative w-full h-full flex flex-col items-center pt-3\" style=\"view-transition-name:main\"><h1 class=\"text-2xl font-semibold text-cyan-400\" style=\"view-transition-name:header1\">Gemini Chat</h1>")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<main class=\"relative w-full h-full flex flex-col items-center pt-3\" style=\"view-transition-name:main\"><h1 class=\"text-2xl font-semibold text-cyan-700 dark:text-cyan-400\" style=\"view-transition-name:header\">Gemini Chat</h1>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
@@ -181,7 +181,7 @@ func section(conversations []models.ChatConversation, isOob bool) templ.Componen
 		}
 		ctx = templ.ClearChildren(ctx)
 		if isOob {
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "<section id=\"section\" class=\"w-11/12 shrink-0 mx-auto h-[68vh] flex flex-col gap-2 overflow-x-hidden overflow-y-auto scroll-smooth scrollbar-thin scrollbar-track-gray-300 scrollbar-thumb-lime-500 mt-2 py-1 px-3 pb-20 rounded lg:w-10/12 lg:mt-10 lg:py-2 lg:px-4 lg:pb-20 xl:w-9/12 xl:h-[72vh] focus:ring-1 focus:ring-slate-200\" style=\"view-transition-name:section\" hx-swap-oob=\"true\">")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "<section id=\"section\" class=\"w-11/12 shrink-0 mx-auto h-[68vh] flex flex-col gap-2 overflow-x-hidden overflow-y-auto scroll-smooth scrollbar-thin scrollbar-track-gray-300 scrollbar-thumb-lime-500 mt-2 py-1 px-3 pb-20 rounded  focus:ring-1 focus:ring-slate-600 dark:focus:ring-slate-200 lg:w-10/12 lg:mt-10 lg:py-2 lg:px-4 lg:pb-20 xl:w-9/12 xl:h-[72vh]\" style=\"view-transition-name:section\" hx-swap-oob=\"true\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
@@ -203,7 +203,7 @@ func section(conversations []models.ChatConversation, isOob bool) templ.Componen
 				return templ_7745c5c3_Err
 			}
 		} else {
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "<section id=\"section\" class=\"w-11/12 shrink-0 mx-auto h-[68vh] flex flex-col gap-2 overflow-x-hidden overflow-y-auto scroll-smooth scrollbar-thin scrollbar-track-gray-300 scrollbar-thumb-lime-500 mt-2 py-1 px-3 pb-20 rounded lg:w-10/12 lg:mt-10 lg:py-2 lg:px-4 lg:pb-20 xl:w-9/12 xl:h-[72vh] focus:ring-1 focus:ring-slate-200\" style=\"view-transition-name:section\">")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "<section id=\"section\" class=\"w-11/12 shrink-0 mx-auto h-[68vh] flex flex-col gap-2 overflow-x-hidden overflow-y-auto scroll-smooth scrollbar-thin scrollbar-track-gray-300 scrollbar-thumb-lime-500 mt-2 py-1 px-3 pb-20 rounded  focus:ring-1 focus:ring-slate-600 dark:focus:ring-slate-200 lg:w-10/12 lg:mt-10 lg:py-2 lg:px-4 lg:pb-20 xl:w-9/12 xl:h-[72vh]\" style=\"view-transition-name:section\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
