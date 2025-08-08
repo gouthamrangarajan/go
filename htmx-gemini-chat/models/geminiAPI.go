44e85d63bc92c7e30727852cfdda37a4cbbd3a4e
gouthamrangarajan <rgouthamraja@yahoo.com>
2025-08-08 15:36:56 -0400

    code refactor

--- htmx-gemini-chat/models/geminiAPI.go
+++ htmx-gemini-chat/models/geminiAPI.go
@@ -12,18 +12,17 @@ type GeminiRequestContent struct {
 	Role  string               `json:"role"`
 	Parts []GeminiRequestParts `json:"parts"`
 }
-type GeminiThinkingConfig struct {
-	Budget int8 `json:"thinkingBudget"`
-}
-type GeminiGenerationConfig struct {
-	Thinking GeminiThinkingConfig `json:"thinkingConfig"`
-}
+
 type GeminiRequest struct {
 	Contents []GeminiRequestContent `json:"contents"`
-	Config   GeminiGenerationConfig `json:"generationConfig"`
+	Config   struct {
+		Thinking struct {
+			Budget int8 `json:"thinkingBudget"`
+		} `json:"thinkingConfig"`
+	} `json:"generationConfig"`
 }
 type GeminiResponse struct {
 	Candidates []struct {
 		Content GeminiRequestContent
-	}
+	} `json:"candidates"`
 }
