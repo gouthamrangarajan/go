063b939fd56a527eb0868872ab369e87f20daf24
gouthamrangarajan <rgouthamraja@yahoo.com>
2025-08-13 17:03:38 -0400

    code refactor

--- datastar-places-to-visit/components/mapCard_templ.go
+++ datastar-places-to-visit/components/mapCard_templ.go
@@ -124,4 +124,79 @@ func PlacesTableRow(spot models.TourismSpots) templ.Component {
 	})
 }
 
+func SetDefaultCheckbox(checked bool, city string, lat string, lng string) templ.Component {
+	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
+			return templ_7745c5c3_CtxErr
+		}
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+		if !templ_7745c5c3_IsBuffer {
+			defer func() {
+				templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err == nil {
+					templ_7745c5c3_Err = templ_7745c5c3_BufErr
+				}
+			}()
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var6 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var6 == nil {
+			templ_7745c5c3_Var6 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		if checked {
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 8, "<div class=\"relative\" id=\"defaultCheckbox\"><div class=\"bg-action-light/50 absolute -top-3 right-3 h-2 rounded-xl w-10 cursor-pointer\" data-on-click=\"$defaultCheckbox.click()\"></div><label class=\"flex gap-2 items-center absolute -top-5.5 right-8 cursor-pointer\"><span class=\"text-primary text-lg\">Default:</span> <input data-ref=\"defaultCheckbox\" type=\"checkbox\" name=\"isDefault\" checked=\"")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			var templ_7745c5c3_Var7 string
+			templ_7745c5c3_Var7, templ_7745c5c3_Err = templ.JoinStringErrs(checked)
+			if templ_7745c5c3_Err != nil {
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/mapCard.templ`, Line: 79, Col: 22}
+			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var7))
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 9, "\" data-on-change=\"")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			var templ_7745c5c3_Var8 string
+			templ_7745c5c3_Var8, templ_7745c5c3_Err = templ.JoinStringErrs("$defaultCheckbox.checked?localStorage.setItem('default', '/map/initialize/" + city + "||" + lat + "||" + lng + "'):localStorage.removeItem('default');")
+			if templ_7745c5c3_Err != nil {
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/mapCard.templ`, Line: 80, Col: 174}
+			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var8))
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 10, "\" class=\"appearance-none outline-none cursor-pointer w-5 h-5 rounded-full bg-action-light/80 transition duration-600 ease-(--ease-elastic-out-5) checked:translate-x-6 checked:bg-action focus:ring-2 focus:ring-action focus:ring-offset-2 focus:ring-offset-action-light\"></label></div>")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+		} else {
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 11, "<div class=\"relative\" id=\"defaultCheckbox\"><div class=\"bg-action-light/50 absolute -top-3 right-3 h-2 rounded-xl w-10 cursor-pointer\" data-on-click=\"$defaultCheckbox.click()\"></div><label class=\"flex gap-2 items-center absolute -top-5.5 right-8 cursor-pointer\"><span class=\"text-primary text-lg\">Default:</span> <input data-ref=\"defaultCheckbox\" type=\"checkbox\" name=\"isDefault\" data-on-change=\"")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			var templ_7745c5c3_Var9 string
+			templ_7745c5c3_Var9, templ_7745c5c3_Err = templ.JoinStringErrs("$defaultCheckbox.checked?localStorage.setItem('default', '/map/initialize/" + city + "||" + lat + "||" + lng + "'):localStorage.removeItem('default');")
+			if templ_7745c5c3_Err != nil {
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/mapCard.templ`, Line: 97, Col: 174}
+			}
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var9))
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 12, "\" class=\"appearance-none outline-none cursor-pointer w-5 h-5 rounded-full bg-action-light/80 transition duration-600 ease-(--ease-elastic-out-5) checked:translate-x-6 checked:bg-action focus:ring-1 focus:ring-action focus:ring-offset-1 focus:ring-offset-transparent\"></label></div>")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+		}
+		return nil
+	})
+}
+
 var _ = templruntime.GeneratedTemplate
