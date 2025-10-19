bd97592dcdcbc774e0b5795aa3d252cf0fde0207
gouthamrangarajan <rgouthamraja@yahoo.com>
2025-10-19 00:59:03 -0400

    show loader when send verification code is clicked

--- datastar-notes/components/login_templ.go
+++ datastar-notes/components/login_templ.go
@@ -90,7 +90,7 @@ func LoginMainEl() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = loader("$verifyingOtp").Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = loader("$verifyingOtp || $_sendingOtp").Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
