335d4e69137c4bb60676accecd1037426384281d
gouthamrangarajan <rgouthamraja@yahoo.com>
2025-11-01 09:35:25 -0400

    code refactor method name chagne

--- htmx-gemini-chat/components/chatInput_templ.go
+++ htmx-gemini-chat/components/chatInput_templ.go
@@ -55,7 +55,7 @@ func chatInput(model models.Section) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<textarea class=\"appearance-none outline-none field-sizing-content min-h-22 max-h-[50vh] w-full py-1 px-3 flex-1 resize-none text-slate-900 dark:text-slate-100 overflow-y-auto scroll-smooth scrollbar-thin scrollbar-track-transparent scrollbar-thumb-lime-500 placeholder:text-gray-500 dark:placeholder:text-gray-400 lg:py-2 lg:px-4\" placeholder=\"Send a message\" rows=\"3\" name=\"prompt\" x-model=\"prompt\" x-on:keyup.enter=\"submitMessage($event,'section')\" x-on:click.stop x-on:paste=\"checkAndHandlePasterForImage($event)\"></textarea>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<textarea class=\"appearance-none outline-none field-sizing-content min-h-22 max-h-[50vh] w-full py-1 px-3 flex-1 resize-none text-slate-900 dark:text-slate-100 overflow-y-auto scroll-smooth scrollbar-thin scrollbar-track-transparent scrollbar-thumb-lime-500 placeholder:text-gray-500 dark:placeholder:text-gray-400 lg:py-2 lg:px-4\" placeholder=\"Send a message\" rows=\"3\" name=\"prompt\" x-model=\"prompt\" x-on:keyup.enter=\"submitMessage($event,'section')\" x-on:click.stop x-on:paste=\"checkAndHandlePasterForImageAndPdf($event)\"></textarea>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
