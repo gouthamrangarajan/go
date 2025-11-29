cc6fa56e1c7b1f85bedcdac9ece06379387387f7
gouthamrangarajan <rgouthamraja@yahoo.com>
2025-11-29 06:36:44 -0500

    changed loader color

--- datastar-claude-chat/components/shared/loader_templ.go
+++ datastar-claude-chat/components/shared/loader_templ.go
@@ -29,7 +29,7 @@ func Loader(class string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		var templ_7745c5c3_Var2 = []any{"h-2 w-2 bg-yellow-500 rounded-full " + class}
+		var templ_7745c5c3_Var2 = []any{"h-2 w-2 bg-yellow-600 rounded-full " + class}
 		templ_7745c5c3_Err = templ.RenderCSSItems(ctx, templ_7745c5c3_Buffer, templ_7745c5c3_Var2...)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
