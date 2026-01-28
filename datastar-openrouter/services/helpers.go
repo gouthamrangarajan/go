758e5ffd62bde08ec091fe9574e6cc3d6a7f7d80
gouthamrangarajan <rgouthamraja@yahoo.com>
2026-01-28 12:51:26 -0500

    copy button proper position and mobile device responsive minor chagne

--- datastar-openrouter/services/helpers.go
+++ datastar-openrouter/services/helpers.go
@@ -32,7 +32,7 @@ const copySvg = `<svg
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 24 24"
 					fill="currentColor"
-					class="size-6 pointer-events-none"
+					class="size-5 pointer-events-none"
 				>
 					<path d="M7.5 3.375c0-1.036.84-1.875 1.875-1.875h.375a3.75 3.75 0 0 1 3.75 3.75v1.875C13.5 8.161 14.34 9 15.375 9h1.875A3.75 3.75 0 0 1 21 12.75v3.375C21 17.16 20.16 18 19.125 18h-9.75A1.875 1.875 0 0 1 7.5 16.125V3.375Z"></path>
 					<path d="M15 5.25a5.23 5.23 0 0 0-1.279-3.434 9.768 9.768 0 0 1 6.963 6.963A5.23 5.23 0 0 0 17.25 7.5h-1.875A.375.375 0 0 1 15 7.125V5.25ZM4.875 6H6v10.125A3.375 3.375 0 0 0 9.375 19.5H16.5v1.125c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V7.875C3 6.839 3.84 6 4.875 6Z"></path>
@@ -186,7 +186,7 @@ func ConvertConversationMarkdownsToHtml(conversations []models.ChatConversation,
 		preRegex := regexp.MustCompile(`<pre`)
 		mkdwn = preRegex.ReplaceAllString(mkdwn, `<div class="relative"><pre`)
 		preEndRegex := regexp.MustCompile(`</pre>`)
-		mkdwn = preEndRegex.ReplaceAllString(mkdwn, `<button class="appearance-none outline-none absolute top-2 right-2 text-white p-1 rounded-full w-8 h-8 flex items-center justify-center cursor-pointer hover:ring-1 hover:ring-white focus:ring-1 focus:ring-white"
+		mkdwn = preEndRegex.ReplaceAllString(mkdwn, `<button class="appearance-none outline-none absolute top-1.5 right-1.5 text-white p-1 rounded-full w-8 h-8 flex items-center justify-center cursor-pointer hover:ring-1 hover:ring-white focus:ring-1 focus:ring-white"
 														alt="Copy to Clipboard"
 														data-on:click__viewtransition="window.navigator.clipboard.writeText((evt.srcElement.previousElementSibling || evt.srcElement.parentElement).innerText.replaceAll('\n\n','\n')).then(()=>{evt.srcElement.innerHTML=document.getElementById('copiedSvg').innerHTML; setTimeout(()=>{evt.srcElement.innerHTML=document.getElementById('copySvg').innerHTML},2000)})">
 													`+copySvg+`
