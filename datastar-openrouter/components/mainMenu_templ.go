610bd4e076aa0a0073feed8f73d7543661d5a8f4
gouthamrangarajan <rgouthamraja@yahoo.com>
2026-01-28 19:07:02 -0500

    increased the padding of menu by two units

--- datastar-openrouter/components/mainMenu_templ.go
+++ datastar-openrouter/components/mainMenu_templ.go
@@ -124,7 +124,7 @@ func MenuItem(session models.ChatSession, searchTerm string) templ.Component {
 		if searchTerm != "" {
 			href += "?search_menu=" + searchTerm
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 6, "<li class=\"w-full flex gap-1 items-center pr-1 hover:bg-gray-200 focus-within:bg-gray-200\" data-class=\"")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 6, "<li class=\"w-full flex gap-1 items-center pr-3 hover:bg-gray-200 focus-within:bg-gray-200\" data-class=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
