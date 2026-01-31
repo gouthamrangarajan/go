ef54b7545ccc9c33905c43d11a4d0ba10c52e0b7
gouthamrangarajan <rgouthamraja@yahoo.com>
2026-01-31 09:49:53 -0500

    code refactor

--- google-drive-content-search/services/authorization.go
+++ google-drive-content-search/services/authorization.go
@@ -13,7 +13,56 @@ import (
 )
 
 // Define the duration 30 days
-const sessionDuration = 30 * 24 * time.Hour
+const SESSION_DURATION = 30 * 24 * time.Hour
+
+const COOKIE_NAME = "drive"
+
+func AuthorizationMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(responseWriter http.ResponseWriter, request *http.Request) {
+		if strings.HasPrefix(request.URL.Path, "/assets") ||
+			(request.URL.Path == "/login" && request.Method == "POST") {
+			next.ServeHTTP(responseWriter, request)
+			return
+		}
+		if validateUserIdInCookie(request) {
+			next.ServeHTTP(responseWriter, request)
+			return
+		}
+
+		if request.Method == "GET" && request.URL.Path == "/" {
+			components.Login().Render(request.Context(), responseWriter)
+			return
+		}
+		http.Error(responseWriter, "Unauthorized", http.StatusUnauthorized)
+	})
+}
+
+func validateUserIdInCookie(r *http.Request) bool {
+	hashKey, err := base64.StdEncoding.DecodeString(os.Getenv("COOKIE_HASH_KEY"))
+	if err != nil {
+		fmt.Printf("Error decoding COOKIE_HASH_KEY: %v\n", err)
+		return false
+	}
+	blockKey, err := base64.StdEncoding.DecodeString(os.Getenv("COOKIE_BLOCK_KEY"))
+	if err != nil {
+		fmt.Printf("Error decoding COOKIE_BLOCK_KEY: %v\n", err)
+		return false
+	}
+
+	newSecureCookie := securecookie.New(hashKey, blockKey)
+
+	if cookie, err := r.Cookie(COOKIE_NAME); err == nil {
+		value := make(map[string]interface{})
+		// This  checks for tampering and expiration automatically
+		if err = newSecureCookie.Decode(COOKIE_NAME, cookie.Value, &value); err == nil {
+			if value["user_id"] == os.Getenv("USER_ID") &&
+				time.Now().Unix()-value["created"].(int64) < int64(SESSION_DURATION.Seconds()) {
+				return true
+			}
+		}
+	}
+	return false
+}
 
 func GenerateUserIdCookie() (http.Cookie, error) {
 	secure := true
@@ -21,7 +70,6 @@ func GenerateUserIdCookie() (http.Cookie, error) {
 		secure = false
 	}
 
-	cookieName := "drive"
 	value := map[string]interface{}{
 		"user_id": os.Getenv("USER_ID"),
 		"created": time.Now().Unix(),
@@ -39,66 +87,20 @@ func GenerateUserIdCookie() (http.Cookie, error) {
 	}
 
 	newSecureCookie := securecookie.New(hashKey, blockKey)
-	cookieValue, err := newSecureCookie.Encode(cookieName, value)
+	cookieValue, err := newSecureCookie.Encode(COOKIE_NAME, value)
 	if err != nil {
 		fmt.Printf("Error encoding cookie: %v\n", err)
 		return http.Cookie{}, err
 	}
 
 	cookie := http.Cookie{
-		Name:     cookieName,
+		Name:     COOKIE_NAME,
 		Value:    cookieValue,
 		Path:     "/",
 		HttpOnly: true,
 		Secure:   secure,
-		MaxAge:   int(sessionDuration.Seconds()),
+		MaxAge:   int(SESSION_DURATION.Seconds()),
 		SameSite: http.SameSiteLaxMode,
 	}
 	return cookie, nil
-}
-func ValidateUserIdInCookie(r *http.Request) bool {
-	hashKey, err := base64.StdEncoding.DecodeString(os.Getenv("COOKIE_HASH_KEY"))
-	if err != nil {
-		fmt.Printf("Error decoding COOKIE_HASH_KEY: %v\n", err)
-		return false
-	}
-	blockKey, err := base64.StdEncoding.DecodeString(os.Getenv("COOKIE_BLOCK_KEY"))
-	if err != nil {
-		fmt.Printf("Error decoding COOKIE_BLOCK_KEY: %v\n", err)
-		return false
-	}
-
-	newSecureCookie := securecookie.New(hashKey, blockKey)
-
-	if cookie, err := r.Cookie("drive"); err == nil {
-		value := make(map[string]interface{})
-		// This  checks for tampering and expiration automatically
-		if err = newSecureCookie.Decode("drive", cookie.Value, &value); err == nil {
-			if value["user_id"] == os.Getenv("USER_ID") &&
-				time.Now().Unix()-value["created"].(int64) < int64(sessionDuration.Seconds()) {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func AuthorizationMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(responseWriter http.ResponseWriter, request *http.Request) {
-		if strings.HasPrefix(request.URL.Path, "/assets") ||
-			(request.URL.Path == "/login" && request.Method == "POST") {
-			next.ServeHTTP(responseWriter, request)
-			return
-		}
-		if ValidateUserIdInCookie(request) {
-			next.ServeHTTP(responseWriter, request)
-			return
-		}
-
-		if request.Method == "GET" && request.URL.Path == "/" {
-			components.Login().Render(request.Context(), responseWriter)
-			return
-		}
-		http.Error(responseWriter, "Unauthorized", http.StatusUnauthorized)
-	})
 }
