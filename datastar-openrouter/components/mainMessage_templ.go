59ba3a8843a35ee2c9777ba7a4de82b2291ef35f
gouthamrangarajan <rgouthamraja@yahoo.com>
2026-02-09 14:24:29 -0500

    changed the sequence of thinking message to last

--- datastar-openrouter/components/mainMessage_templ.go
+++ datastar-openrouter/components/mainMessage_templ.go
@@ -97,15 +97,15 @@ func ChatMessage(message models.ChatConversation, stream bool) templ.Component {
 				return templ_7745c5c3_Err
 			}
 		}
+		templ_7745c5c3_Err = ChatMessageFileData(message).Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		if message.Role == "assistant" && stream {
 			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 8, "<span class=\"animate-pulse opacity-80 text-sm\" id=\"thinkingMesssage\">thinking...</span>")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-		}
-		templ_7745c5c3_Err = ChatMessageFileData(message).Render(ctx, templ_7745c5c3_Buffer)
-		if templ_7745c5c3_Err != nil {
-			return templ_7745c5c3_Err
 		}
 		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 9, "</div>")
 		if templ_7745c5c3_Err != nil {
