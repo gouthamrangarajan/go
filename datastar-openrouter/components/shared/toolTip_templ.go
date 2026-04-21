0d771260857dc702753a930ebe59ee23c0fda401
gouthamrangarajan <rgouthamraja@yahoo.com>
2026-04-21 06:07:07 -0400

    adjusted opacity for tooltip

--- datastar-openrouter/components/shared/toolTip_templ.go
+++ datastar-openrouter/components/shared/toolTip_templ.go
@@ -31,7 +31,7 @@ func ToolTip(data models.UIToolTipModel) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<p class=\"absolute opacity-70\" style=\"")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<p class=\"absolute opacity-80\" style=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
