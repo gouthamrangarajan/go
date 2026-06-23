0bbff9a7d92a3849ac472c3a14b62d2eb6bb905b
gouthamrangarajan <rgouthamraja@yahoo.com>
2026-06-23 18:54:39 -0400

    code refactor

--- datastar-openrouter/components/mainMenu_templ.go
+++ datastar-openrouter/components/mainMenu_templ.go
@@ -176,18 +176,17 @@ func MenuItem(session models.ChatSession, searchTerm string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 10, "\" data-on:click=\"")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 10, "\" data-on:click__prevent=\"")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var8 string
 		templ_7745c5c3_Var8, templ_7745c5c3_Err = templ.JoinStringErrs(`	
-				evt.preventDefault();
 				$sessionId=` + strconv.Itoa(session.Id) + `;
 				@get('` + href + `');
 				`)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/mainMenu.templ`, Line: 132, Col: 5}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/mainMenu.templ`, Line: 131, Col: 5}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var8))
 		if templ_7745c5c3_Err != nil {
@@ -200,7 +199,7 @@ func MenuItem(session models.ChatSession, searchTerm string) templ.Component {
 		var templ_7745c5c3_Var9 string
 		templ_7745c5c3_Var9, templ_7745c5c3_Err = templ.JoinStringErrs(session.Title)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/mainMenu.templ`, Line: 135, Col: 18}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/mainMenu.templ`, Line: 134, Col: 18}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var9))
 		if templ_7745c5c3_Err != nil {
@@ -217,7 +216,7 @@ func MenuItem(session models.ChatSession, searchTerm string) templ.Component {
                 $showDeleteModal=true;
             `)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/mainMenu.templ`, Line: 143, Col: 13}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/mainMenu.templ`, Line: 142, Col: 13}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var10))
 		if templ_7745c5c3_Err != nil {
