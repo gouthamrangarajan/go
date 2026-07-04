46c4c3749b203fc9ffa06cfb31fd72dbbf6318c7
gouthamrangarajan <rgouthamraja@yahoo.com>
2026-07-03 20:27:07 -0400

    padding fix week calendar date

--- datastar-calendar/components/weekCalendarTable_templ.go
+++ datastar-calendar/components/weekCalendarTable_templ.go
@@ -103,7 +103,7 @@ func weekCalendarTable(data models.WeekCalendarData) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "\" style=\"view-transition-name:none\"><div class=\"xl:pt-10\">")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "\" style=\"view-transition-name:none\"><div class=\"pt-4 xl:pt-10\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
