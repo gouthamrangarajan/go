f89b0c5eae7f32f632472173668b7fb44115252b
gouthamrangarajan <rgouthamraja@yahoo.com>
2026-07-19 20:15:25 -0400

    added response header write to solve dropping sse problem feedback from gemini model

--- datastar-calendar/services/routeHandlers.go
+++ datastar-calendar/services/routeHandlers.go
@@ -146,6 +146,12 @@ func SSEHandler(responseWriter http.ResponseWriter, request *http.Request) {
 	uiSidMap.Store(sessionKey, userSessionChannel)
 	defer uiSidMap.CompareAndDelete(sessionKey, userSessionChannel)
 
+	responseWriter.Header().Set("Content-Type", "text/event-stream")
+    responseWriter.Header().Set("Cache-Control", "no-cache")
+    responseWriter.Header().Set("Connection", "keep-alive")
+    // This header tells Nginx/Railway Proxy not to buffer the stream
+    responseWriter.Header().Set("X-Accel-Buffering", "no") 
+	
 	sse := datastar.NewSSE(responseWriter, request)
 
 	heartBeatTicker := time.NewTicker(5 * time.Second)
